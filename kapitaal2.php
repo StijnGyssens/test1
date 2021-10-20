566dfe556124c44f5e0de7c8d3fcb9b6f21023b4
StijnGyssens <stijn.gyssens@gmail.com>
2021-10-20 19:45:39 +0200

    uitleg bij verbeterde oef kapitaal2

--- kapitaal2.php
+++ kapitaal2.php
@@ -1,13 +1,14 @@
 <?php
-
+//aantal spaties uitrekenen
 function Rechtsuitlijnen($str,$length){
+    $sp=[];
     $space=$length- strlen($str);
     for ($i=0;$i<$space;$i++){
-        print " ";
+        array_push($sp," ");
     }
-    print $str;
+    return join("",$sp).$str;
 }
-
+//vragen achter gegevens
 $kap = readline("Wat is het kapitaal? ");
 $int = readline("Wat is de intrest in procente? ");
 $jaren = readline("Wat is de looptijd? ");
@@ -15,16 +16,20 @@ $limiet = readline("Is er een limiet? ");
 $lim=null;
 if ($limiet==="y"){$lim = readline("Wat is de limiet? ");}
 
+
+//intrest berekenen
 function intrest($kap,$int,$jaren,$lim){
     $l=14;
+    //header maken
     $head_jaar=RechtsUitlijnen("Jaar",$l);
     $head_kapitaal=RechtsUitlijnen("Kapitaal",$l);
     $head_rente=RechtsUitlijnen("Rente",$l);
     print "$head_jaar$head_kapitaal$head_rente\n";
 
+    //inhoud
     for ($i=1;$i<=$jaren;$i++){
-        $over=($lim and $kap>$lim);
-        if ($over)break;
+        $over=($lim and $kap>$lim); //is true als er een $lim is en als de $kap groter is dan $lim
+        if ($over)break;  //stop als $over true is
         $rente=$kap*$int/100;
 
         $jaar=RechtsUitlijnen($i,$l);
